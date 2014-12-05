ed0557b7f5a7aa215f1de368022f3c72ffc035b7
Autoritaire <aypek.tolga@gmail.com>
2014-12-05 16:47:33 +0100

    Create ChoixDifficulté.java
    à associer avec le Listener du choix de la difficulté. Si la difficulté "Facile" est cochée, la difficulté prend la valeur 0. "Moyen" 1 et "Difficile" 2. Truc très con.

---
+++ ChoixDifficulté.java
@@ -0,0 +1,24 @@
+
+
+public class ChoixDifficulté {
+
+	int difficulté;
+
+	//Listener du Checkbox ou bouton de difficulté
+
+	if(/*Checkbox ou Bouton*/ == "Facile"){
+		Difficulté = 0;
+	}
+	else(/*Checkbox ou Bouton*/ == "Moyen"){
+		Difficulté = 1;
+	}
+	else(/*Checkbox ou Bouton*/ == "Difficile"){
+		Difficulté = 2;
+	}
+
+
+	public int getDifficulté(){
+		return difficulté;
+	}
+
+}
